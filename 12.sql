f0b7dd77f8751d8da4c5cdc54ae81658ad4861ae
0110zzl <86716912+0110zzl@users.noreply.github.com>
2021-07-01 08:58:08 +0800

    Add files via upload

---
+++ 12.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Enterprise v12.08 (64 bit)
+MySQL - 5.0.41-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `reply_detail` (
+	`id` int (11),
+	`invid` int (11),
+	`content` varchar (1500),
+	`author` varchar (150),
+	`createdate` date 
+); 
